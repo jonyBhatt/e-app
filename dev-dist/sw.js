a3c6ad3e078c2f4cb2fb171b50d1fb2c95faa0f0
jonyBhatt <jonybhattacharjee886@gmail.com>
2026-05-15 14:09:34 +0600

    fix  style

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kn14ok2r0jo"
+    "revision": "0.423ilu2o29k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
